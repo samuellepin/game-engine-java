417d76524f71a522ba88cbd6fd62085d6aa33058
David Darras <david.darras1308@gmail.com>
2024-06-05 16:27:16 +0200

    [fix] src.AI - Action, Condition, Transition, etc.

--- project.snake/src/AI/Automaton.java
+++ project.snake/src/AI/Automaton.java
@@ -1,6 +1,28 @@
 package src.AI;
 
+import java.util.ArrayList;
+
+import src.Entity;
+
 public class Automaton
 {
+  private ArrayList< Transition > m_transitions;
 
+  public Automaton( ArrayList< Transition > transitions )
+  {
+    m_transitions = transitions;
+  }
+
+  public boolean Next( Entity entity )
+  {
+    for ( Transition transition : m_transitions )
+    {
+      if( transition.getSource().isEqual( entity.getState() ) && transition.getCondition().evaluate( entity ) )
+      {
+        transition.getAction().run( entity );
+        return true;
+      }
+    }
+    return false;
+  }
 }
