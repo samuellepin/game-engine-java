ca65f4a06fda4521a83d0b80152d5f7752afadc7
Grégory Toureille <gregory.toureille@etu.univ-grenoble-alpes.fr>
2024-06-06 11:54:11 +0200

    Revert "tick on children entities implemented"
    This reverts commit 20cde206a73c748af32ce7200e5363f9094edd9a.

--- project.snake/src/Model.java
+++ project.snake/src/Model.java
@@ -2,13 +2,8 @@ package src;
 
 public class Model
 {
-  private java.util.List< Entity > m_entities;
-  
   void tick( int elapsed )
   {
-    java.util.ListIterator< Entity > iter = m_entities.listIterator();
-    
-    while (iter.hasNext())
-      iter.next().tick( elapsed );
+    throw new RuntimeException( "NYI" );
   }
 }
