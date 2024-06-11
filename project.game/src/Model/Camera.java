f395afaaa9c3476dc3a25c89f4e23427fbb3f56e
Remi Gaulmin <remi.gaulmin@etu.univ-grenoble-alpes.fr>
2024-06-11 10:21:11 +0200

    Class Camera defined but NYI

---
+++ project.game/src/Model/Camera.java
@@ -0,0 +1,34 @@
+package src.Model;
+
+public class Camera
+{
+  public Vector getPos()
+  {
+    throw new RuntimeException( "NYI" );
+  }
+
+  public double getWidth()
+  {
+    throw new RuntimeException( "NYI" );
+  }
+
+  public double getHeight()
+  {
+    throw new RuntimeException( "NYI" );
+  }
+
+  public void setPos( Vector v )
+  {
+    throw new RuntimeException( "NYI" );
+  }
+
+  public void setWidth()
+  {
+    throw new RuntimeException( "NYI" );
+  }
+
+  public void setHeight()
+  {
+    throw new RuntimeException( "NYI" );
+  }
+}
