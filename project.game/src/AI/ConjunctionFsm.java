c6505ccc29de7e77bd340d19c9cb89309068b75a
Romain PRAT <Romain.Prat1@etu.univ-grenoble-alpes.fr>
2024-06-18 10:56:05 +0200

    Conjunction and Disjunction

---
+++ project.game/src/AI/ConjunctionFsm.java
@@ -0,0 +1,19 @@
+package src.AI;
+
+import src.Model.Entity;
+
+public class ConjunctionFsm implements ConditionFsm
+{
+  ConditionFsm m_c1, m_c2;
+
+  ConjunctionFsm( ConditionFsm c1, ConditionFsm c2 )
+  {
+    m_c1 = c1;
+    m_c2 = c2;
+  }
+
+  public boolean evaluate( Entity e )
+  {
+    return m_c1.evaluate( e ) && m_c2.evaluate( e );
+  }
+}
