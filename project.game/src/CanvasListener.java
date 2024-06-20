c5d6d6bd1b4a809ceea151cd133d77565d4f4c39
David Darras <david.darras1308@gmail.com>
2024-06-20 19:06:13 +0200

    [add] Controller - Mouse

--- project.game/src/CanvasListener.java
+++ project.game/src/CanvasListener.java
@@ -11,127 +11,76 @@ import src.Model.Model;
 public class CanvasListener implements GameCanvasListener
 {
   private Game       m_game;
-  private Model      m_model;
   private Controller m_controller;
 
   CanvasListener( Game game )
   {
     m_game = game;
-    m_model = Model.getInstance();
     m_controller = Controller.getInstance();
   }
 
   @Override
   public void mouseClicked( MouseEvent e )
   {
-//    System.out.println( "Mouse clicked: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
   }
 
   @Override
   public void mousePressed( MouseEvent e )
   {
-//    System.out.println( "Mouse pressed: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
+    m_controller.updateMouseButtonPressed( e );
   }
 
   @Override
   public void mouseReleased( MouseEvent e )
   {
-//    System.out.println( "Mouse released: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
+    m_controller.updateMouseButtonReleased( e );
   }
 
   @Override
   public void mouseEntered( MouseEvent e )
   {
-//    System.out.println( "Mouse entered: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
+    m_controller.updateMousePos( e );
   }
 
   @Override
   public void mouseExited( MouseEvent e )
   {
-//    System.out.println( "Mouse exited: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
+    m_controller.updateMousePos( e );
   }
 
   @Override
   public void mouseDragged( MouseEvent e )
   {
-//    System.out.println( "Mouse dragged: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
+    m_controller.updateMousePos( e );
   }
 
   @Override
   public void mouseMoved( MouseEvent e )
   {
-//    System.out.println( "Mouse moved: (" + e.getX() + "," + e.getY() + ")" );
-//    System.out.println( "   modifiers=" + e.getModifiersEx() );
-//    System.out.println( "   buttons=" + e.getButton() );
+    m_controller.updateMousePos( e );
   }
 
   @Override
   public void keyTyped( KeyEvent e )
   {
-//    System.out.println( "Key typed: " + e.getKeyChar() + " code=" + e.getKeyCode() );
   }
 
   @Override
   public void keyPressed( KeyEvent e )
   {
-    System.out.println( "Key pressed: " + e.getKeyChar() + " code=" + e.getKeyCode() );
-    
     m_controller.updateKeyPressed( e );
-
-    double orientations[] = { -Math.PI, -Math.PI / 2, 0, Math.PI / 2 };
-    int    code           = e.getKeyCode();
-    Entity player1        = Model.getInstance().getPlayer1();
-    switch ( code )
-    {
-    case 37: // LEFT
-    case 38: // UP
-    case 39: // RIGHT
-    case 40: // DOWN
-      player1.turn( orientations[ code - 37 ] );
-      player1.doMove( player1.getOrientation() );
-      player1.setIsMoving( true );
-      break;
-    }
-
   }
 
   @Override
   public void keyReleased( KeyEvent e )
   {
     m_controller.updateKeyReleased( e );
-
-    int    code    = e.getKeyCode();
-    Entity player1 = Model.getInstance().getPlayer1();
-    switch ( code )
-    {
-    case 37: // LEFT
-    case 38: // UP
-    case 39: // RIGHT
-    case 40: // DOWN
-      player1.setIsMoving( false );
-      break;
-    }
-
-    // System.out.println( "Key released: " + e.getKeyChar() + " code=" +
-    // e.getKeyCode() );
   }
 
   @Override
   public void tick( long elapsed )
   {
-    m_controller.poll();
+    m_controller.update();
     m_game.tick( elapsed );
   }
 
